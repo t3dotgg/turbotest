ebca205ce3e8d5aec067ecaa3a86a4565b891baf
Theo Browne <me@t3.gg>
2023-04-18 23:52:51 -0700

    one more to confirm

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Docs() {
   return (
     <div>
-      <h1>Docs - random change 2</h1>
+      <h1>Docs - random change 3</h1>
       <Button />
     </div>
   );
